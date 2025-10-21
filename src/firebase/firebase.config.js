aa869c7ca2824cb21e967f96cb8c0d92394044b9
Yeasinoncode98 <devoncode98@gmail.com>
2025-10-22 03:24:35 +0600

    Added firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -39,16 +39,38 @@
 
 // export default app;
 
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCTMx9FjfVmSYIgEpVtBJZv_44kPvZ06eA",
+//   authDomain: "green-nest-store-and-shop.firebaseapp.com",
+//   projectId: "green-nest-store-and-shop",
+//   storageBucket: "green-nest-store-and-shop.firebasestorage.app",
+//   messagingSenderId: "818726122955",
+//   appId: "1:818726122955:web:ee0f0f6a474399afef724e",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Auth and Google provider
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
+
+// export default app;
+
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTMx9FjfVmSYIgEpVtBJZv_44kPvZ06eA",
-  authDomain: "green-nest-store-and-shop.firebaseapp.com",
-  projectId: "green-nest-store-and-shop",
-  storageBucket: "green-nest-store-and-shop.firebasestorage.app",
-  messagingSenderId: "818726122955",
-  appId: "1:818726122955:web:ee0f0f6a474399afef724e",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
