3bf1b9351c9bf175d0a8698e68ad23c5370fa81c
Yeasinoncode98 <devoncode98@gmail.com>
2025-10-24 01:40:40 +0600

    Fixed the privatRouter.jsx

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,24 +1,3 @@
-// // src/Components/PrivateRoute.jsx
-// import React, { useContext } from "react";
-// import { Navigate, useLocation } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContext";
-
-// const PrivateRoute = ({ children }) => {
-//   const { user, loading } = useContext(AuthContext);
-//   const location = useLocation();
-
-//   if (loading) return <div className="p-8 text-center">Loading...</div>;
-
-//   if (!user) {
-//     // Redirect to login and keep the intended location in state
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-
-//   return children;
-// };
-
-// export default PrivateRoute;
-
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
